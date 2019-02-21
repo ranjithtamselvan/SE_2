4215721c1f9a2d07af054ae1fb489b3e793712fb
ranjithSE <ranjithtamizhchelvan@gmail.com>
2019-02-21 07:19:28 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,33 +18,61 @@ class MoviesController < ApplicationController
 #      @selected_col = params[:sorted_by]
 #    end
 
-    if params[:sorted_by]==nil
-      if params[:ratings]
-        @movies = Movie.where({rating: params[:ratings].keys})
-      else
-        @movies = Movie.all
-      end 
-    else
-      if params[:ratings]
-        @movies = Movie.where({rating: params[:ratings].keys}).order(params[:sorted_by])
-        @selected_col = params[:sorted_by]
-      else
-        @movies = Movie.order(params[:sorted_by])
-        @selected_col = params[:sorted_by]
-      end
-    end
-    @all_ratings = Movie.returnRatings
+#    if params[:sorted_by]==nil
+#      if params[:ratings]
+#        @movies = Movie.where({rating: params[:ratings].keys})
+#      else
+#        @movies = Movie.all
+#      end 
+#    else
+#      if params[:ratings]
+#        @movies = Movie.where({rating: params[:ratings].keys}).order(params[:sorted_by])
+#        @selected_col = params[:sorted_by]
+#      else
+#        @movies = Movie.order(params[:sorted_by])
+#        @selected_col = params[:sorted_by]
+#      end
+#    end
+#    @all_ratings = Movie.returnRatings
     
     
-    @checked_ratingboxes = params[:ratings]
-    if @checked_ratingboxes
-      @flag=false   #check whether this is the first load of page. Hash for rating_boxes=NULL
-    else
-      @flag=true    #create a new Hash for rating boxes
-      @checked_ratingboxes = Hash.new 
+#    @checked_ratingboxes = params[:ratings]
+#    if @checked_ratingboxes
+#      @flag=false   #check whether this is the first load of page. Hash for rating_boxes=NULL
+#    else
+#      @flag=true    #create a new Hash for ratingboxes 
+#      @checked_ratingboxes = Hash.new 
+#    end
+        @all_ratings = Movie.returnRatings
+    
+
+    if params[:ratings]
+      session[:checked_ratingboxes] = params[:ratings]
+    elsif !session[:checked_ratingboxes]   
+      session[:checked_ratingboxes] = Hash.new
+      @all_ratings.each do |item| session[:checked_ratingboxes][item] = 1 end
+    end  
+
+    @checked_ratingboxes = session[:checked_ratingboxes]
+
+    
+    if params[:sorted_by]
+      session[:sorted_by] = params[:sorted_by]
     end
-      
-      
+    
+    @selected_col = params[:sorted_by]
+    
+    @movies = Movie.where({rating:  session[:checked_ratingboxes].keys })
+
+    
+    if session[:sorted_by]
+      @movies = @movies.order(session[:sorted_by])
+    end
+
+    redir_url_hash = {'ratings'=> session[:checked_ratingboxes], 'sorted_by'=> session[:sorted_by]}
+    if ( session[:checked_ratingboxes] != params[:ratings] || session[:sorted_by] != params[:sorted_by] ) #Redirect When URI doesnt match session params
+      redirect_to(redir_url_hash)
+    end
   end
 
   def new
